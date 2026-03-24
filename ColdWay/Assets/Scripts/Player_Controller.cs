08948008da4259386e56276d387c8370a5fa0921
Şaban <sabanulkuseven56@gmail.com>
2026-03-25 01:23:31 +0300

    bolge1 harita düzeni
    bazı mekanikler,harita şekilleri

--- ColdWay/Assets/Scripts/Player_Controller.cs
+++ ColdWay/Assets/Scripts/Player_Controller.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    float yuruyushýzý = 2f;
-    float kosmahýzý = 5f;
+    public float yuruyusHizi = 2f;
+    public float kosmaHizi = 5f;
+    public bool kosmakAktif = true; // ? ekle
 
-    public float horizontal;
-    public float vertical;
-    float deger;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        //Koþmanýn kontrol edileceði bölge
-        bool kosuyormu = Input.GetKey(KeyCode.LeftShift);
-        deger = kosuyormu ? kosmahýzý : yuruyushýzý;
+        // Koþma kontrolü
+        bool kosuyorMu = Input.GetKey(KeyCode.LeftShift)
+                         && kosmakAktif;
+        float hiz = kosuyorMu ? kosmaHizi : yuruyusHizi;
 
-        deger *= Time.deltaTime;
+        transform.Translate(
+            horizontal * hiz * Time.deltaTime,
+            0f,
+            vertical * hiz * Time.deltaTime,
+            Space.Self
+        );
+    }
 
-        //Karakterin gideceði yöne göre hareketlenme bölgesi
-        transform.Translate(horizontal * deger, 0f, vertical * deger,Space.Self);
+    public bool KosuyorMu()
+    {
+        return Input.GetKey(KeyCode.LeftShift) && kosmakAktif;
     }
 }
