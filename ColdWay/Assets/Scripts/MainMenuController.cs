d50401deec097dd9d534ff7e4784975a57bb8d6d
zarqof <ultima_58@hotmail.com>
2026-06-07 14:03:27 +0300

    Main Menu Script Olusturuldu
    -Main Menu duzenlendi

--- ColdWay/Assets/Scripts/MainMenuController.cs
+++ ColdWay/Assets/Scripts/MainMenuController.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Sahneleri yüklemek için bu kütüphane þarttýr
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Hayatta kalma oyununun olduðu sahnenin tam adýný buraya yazacaðýz
+    // sahnenin tam adýný yaz
     [SerializeField] private string gameplaySceneName = "GameScene";
+
+    public GameObject menuArea;
+    public GameObject optionsPanel;
 
     public void NewGame()
     {
         Debug.Log("Oyun yükleniyor...");
-        // Zaman donuk kaldýysa (Pause menüsünden ötürü) normal akýþýna döndür
+        // normal akýþýna döndür
         Time.timeScale = 1f;
 
-        // Ýsmini verdiðimiz oyun sahnesini yükle
+        
         SceneManager.LoadScene(gameplaySceneName);
+    }
+
+    // AYARLAR butonuna basýnca çalýþacak
+    public void OpenOptions()
+    {
+        menuArea.SetActive(false);    // Ana butonlarý gizle
+        optionsPanel.SetActive(true); // Ayarlar panelini göster
+    }
+
+    // GERÝ butonuna basýnca çalýþacak
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false); // Ayarlar panelini gizle
+        menuArea.SetActive(true);      // Ana butonlarý geri getir
     }
 
     public void QuitGame()
